d08441c9e6eb8ab5e48a1ac257d8ea5798b2494e
itsmekylie <122257319+itsmekylie@users.noreply.github.com>
2025-04-07 22:46:00 +0800

    Update settings.py

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'todo_backend.urls'
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000/",
     "https://itsmekylie.github.io/",
-     "http://localhost:8000/",
+    "http://localhost:8000/",
 ]
 
 
